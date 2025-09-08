edb0a1a8ffb17c8f27f70a5323fc5c659f47e489
Abhinand Shashendran <144478707+ab-inand@users.noreply.github.com>
2025-09-08 15:28:57 +0530

    Update Q1.cpp

--- DAY_1/Q1.cpp
+++ DAY_1/Q1.cpp
@@ -1,1 +1,15 @@
+//Check if a number is even or odd
 
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cout<<"Entre a number"<<endl;
+    cin>>a;
+    if(a%2==0)
+        cout << "EVEN";
+    else
+        cout<<"ODD";
+
+    return 0;
+    }
